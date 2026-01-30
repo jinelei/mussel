d28194adae22f68a7046aa4e50378a849b87bd11
jinelei <jinelei@163.com>
2026-01-30 16:06:33 +0800

    优化图标保存1

--- src/pages/Navigation.tsx
+++ src/pages/Navigation.tsx
@@ -26,7 +26,6 @@ const Navigation: React.FC = () => {
             } else {
                 const textArea = document.createElement('textarea');
                 textArea.value = text;
-                // 隐藏textarea（避免页面闪烁）
                 textArea.style.position = 'fixed';
                 textArea.style.opacity = '0';
                 document.body.appendChild(textArea);
