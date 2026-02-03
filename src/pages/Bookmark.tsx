fe80589d6dd857fd7ec368e2e339aceebd00038c
jinelei <jinelei@163.com>
2026-02-03 19:43:35 +0800

    书签管理临时保存

--- src/pages/Bookmark.tsx
+++ src/pages/Bookmark.tsx
@@ -1,6 +1,60 @@
-const Bookmark = () => {
+import {useEffect, useState} from "react";
+import {BookmarkDomain, Service} from "../api";
+import {useLocation} from "react-router-dom";
+import {Typography} from "antd";
+
+const Bookmark: React.FC = () => {
+    const location = useLocation();
+    const [bookmarks, setBookmarks] = useState<BookmarkDomain[]>();
+
+    useEffect(() => {
+        Service.bookmarkTree({})
+            .then(res => {
+                if (200 === res.code) {
+                    setBookmarks(res.data);
+                }
+            }).catch(reason => {
+            console.warn(reason)
+        })
+    }, [location.pathname]);
     return (
-        <div>这里是书签</div>
+        <div>
+            {bookmarks?.map(it => {
+                return (
+                    <div style={{
+                        backgroundColor: 'gray',
+                        margin: '1rem',
+                        padding: '1rem',
+                    }}>
+                        <Typography.Text>{it.name}</Typography.Text>
+                        <hr/>
+                        <div style={{
+                            display: 'flex',
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                        }}>
+                            {(it.children || []).map(iit => {
+                                return <div style={{
+                                    display: 'flex',
+                                    flexDirection: 'column',
+                                    alignItems: 'center',
+                                    justifyContent: 'center',
+                                    backgroundColor: 'white',
+                                    margin: '1rem',
+                                    padding: '1rem',
+                                }}
+                                >
+                                    <Typography.Text type={'success'}>{iit.name}</Typography.Text>
+                                    <Typography.Text type={'secondary'}>{iit.url}</Typography.Text>
+                                    <Typography.Text type={'secondary'}>{iit.icon}</Typography.Text>
+                                    <br/>
+                                </div>
+                            })}
+                        </div>
+                    </div>
+                )
+            })}
+        </div>
     )
 }
 
